106b0b53237fc871220f9ea9313b38314baef879
Maurice Wipf <maurice@gmx.de>
2025-11-10 10:28:42 +0100

    Update icons

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
             react(),
             VitePWA({
                 registerType: 'autoUpdate',
-                includeAssets: ['icon-favicon.png'],
+                includeAssets: ['icon-favicon-32x32.png', 'icon-favicon-16x16.png'],
                 manifest: {
                     name: 'AI Chat Translator',
                     short_name: 'AI Translator',
